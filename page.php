fc1468d77136cc8931c31bb73891e55074e94646
doyo91 <doyo_91@hotmail.com>
2020-10-28 12:53:31 +0100

    Functions Page & Contact Page

--- page.php
+++ page.php
@@ -18,30 +18,35 @@ get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 
 ?>
+<!-- <header class="page-header entry-header">
+	<div class="container">
+		<?php the_title( '<h1 class="entry-title">' , '</h1>'); ?>
+	</div>
+</header> -->
 
 <div class="wrapper" id="page-wrapper">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+	<div class="container" id="content" tabindex="-1">
 
 		<div class="row">
 
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
-
 			<main class="site-main" id="main">
+				<div class="row">
+					<div class="col-lg-1"></div>
+					<div class="col-lg-10">
+							<?php
+						while ( have_posts() ) {
+							the_post();
+							get_template_part( 'loop-templates/content', 'page' );
 
-				<?php
-				while ( have_posts() ) {
-					the_post();
-					get_template_part( 'loop-templates/content', 'page' );
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
-				}
-				?>
-
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) {
+								comments_template();
+							}
+						}
+						?>
+					</div>
+				</div>
 			</main><!-- #main -->
 
 			<!-- Do the right sidebar check -->
